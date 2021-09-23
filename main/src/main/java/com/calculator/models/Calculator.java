f59d741405e85b2f923ad02d3a0e19d8719c5ec8
ChrisQA-GB <cyiangou@qa.com>
2021-09-23 19:14:09 +0100

    Created Very Basic Java Calculator Class For Math

---
+++ main/src/main/java/com/calculator/models/Calculator.java
@@ -0,0 +1,25 @@
+package com.calculator.models;
+
+public class Calculator {
+
+    public int addTwoNumbers(int input1, int input2) {
+        int sumOfInput = input1 + input2;
+        return sumOfInput;
+    }
+
+    public int subtractTwoNumbers(int input1, int input2) {
+        int sumOfInput = input1 - input2;
+        return sumOfInput;
+    }
+
+    public int divideTwoNumbers(int input1, int input2) {
+        int sumOfInput = input1 / input2;
+        return sumOfInput;
+    }
+
+        public int multiplyTwoNumbers(int input1, int input2) {
+            int sumOfInput = input1 * input2;
+            return sumOfInput;
+    
+}
+}
