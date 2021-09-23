9f5f09b260d7981fa4da1929c5783e8e2fe1738e
ChrisQA-GB <cyiangou@qa.com>
2021-09-23 19:16:20 +0100

    Added comments to Calculator file

--- main/src/main/java/com/calculator/models/Calculator.java
+++ main/src/main/java/com/calculator/models/Calculator.java
@@ -2,6 +2,9 @@ package com.calculator.models;
 
 public class Calculator {
 
+/* Our plan is to create a fully fledged calculator,
+ for now this will act as the most basic template */
+
     public int addTwoNumbers(int input1, int input2) {
         int sumOfInput = input1 + input2;
         return sumOfInput;
